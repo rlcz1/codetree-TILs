d79048deeb849bd39783e880a3ac6e726054f5a2
Gyuri <86146265+rlcz1@users.noreply.github.com>
2024-02-02 20:57:40 +0900

    Title: 사각형 채우기 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240202/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define MAX 1001
+#define MOD 1000000007
+
+using namespace std;
+
+int main() {
+    int dp[MAX] = {0,};
+    int n;
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 7;
+    dp[3] = 22;
+
+    for (int i = 4; i < MAX; i++) {
+        dp[i] = (2 * dp[i-1]) + (3 * dp[i-2]) % MOD;
+        for (int j = i - 3; j >= 0; j--) {
+            dp[i] = (dp[i] + 2 * dp[j]) % MOD;
+        }
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
