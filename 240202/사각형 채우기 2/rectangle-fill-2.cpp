c32d42b1f749728b946358b809a56c6647a01598
Gyuri <86146265+rlcz1@users.noreply.github.com>
2024-02-02 21:22:36 +0900

    Title: 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240202/사각형 채우기 2/rectangle-fill-2.cpp
+++ 240202/사각형 채우기 2/rectangle-fill-2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #define MAX 1001
+#define MOD 10007
+
 using namespace std;
 
 int main() {
@@ -12,7 +14,7 @@ int main() {
     dp[2] = 3;
 
     for (int i = 3; i <= n; i++) {
-        dp[i] = dp[i-1] + (2 * dp[i-2]);
+        dp[i] = (dp[i-1] + (2 * dp[i-2])) % MOD;
     }
 
     cout << dp[n];
